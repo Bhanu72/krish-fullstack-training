ad9a6fa7cbdae84aed164d91c0797134b189e387
Bhanu72 <mleobhanu@gmail.com>
2022-04-01 23:13:19 +0530

    Lamda Regular 2

---
+++ src/com/company/Main.java
@@ -0,0 +1,11 @@
+package com.company;
+
+public class Main {
+
+    public static void main(String[] args) {
+	Calculater s = new Sum();
+    s.sum(10,20);
+
+        System.out.println(s.sum(10,20));
+    }
+}
---
+++ src/com/company/Sum.java
@@ -0,0 +1,9 @@
+package com.company;
+
+public class Sum implements Calculater{
+
+    @Override
+    public int sum(int x, int y) {
+        return x+y;
+    }
+}
